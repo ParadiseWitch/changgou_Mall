db69828d1bc86b88161cd374b6d36bb513bc06ef
Maiiiiiid <zhp92@outlook.com>
2020-09-17 00:36:16 +0800

    开启熔断 信号量隔离

--- changgouparent/changgou-common/src/main/java/entity/FeignInterceptor.java
+++ changgouparent/changgou-common/src/main/java/entity/FeignInterceptor.java
@@ -19,6 +19,10 @@ public class FeignInterceptor implements RequestInterceptor {
 
 	@Override
 	public void apply(RequestTemplate requestTemplate) {
+		//记录了当前用户请求的所有数据,包含请求头和请求参数等
+		//当前用户请求的时候对应线程的数据
+		//如果开启了熔断, 这部分在Feign调用在新的线程, 无法获取之前线程的数据, requestAttributes=null
+		//解决办法  将熔断策略换成 信号量隔离  此时不会开启新线程
 		ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
 		Enumeration<String> headerNames = requestAttributes.getRequest().getHeaderNames();
 
