50b11ca47a28b6ee3b7e4cfdf50b1d8ee5754a51
Maiiiiiid <zhp92@outlook.com>
2020-09-23 12:52:24 +0800

    TTL DLX实现延时队列 支付超时(10s)时关闭支付

---
+++ changgouparent/changgou-service/changgou-service-order/src/main/java/com/changgou/order/mq/listener/DelayMessageListener.java
@@ -0,0 +1,11 @@
+package com.changgou.order.mq.listener;
+
+/**
+ * @ClassName com.changgou.order.mq.listener.DelatMessageListener
+ * @Description
+ * @Author Maid
+ * @Date 2020/9/22 0022 18:17
+ * @Version v1.0
+ */
+public class DelatMessageListener {
+}
---
+++ changgouparent/changgou-service/changgou-service-order/src/main/java/com/changgou/order/mq/queue/QueueConfig.java
@@ -0,0 +1,11 @@
+package com.changgou.order.mq.queue;
+
+/**
+ * @ClassName com.changgou.order.mq.queue.QueueConfig
+ * @Description
+ * @Author Maid
+ * @Date 2020/9/22 0022 17:51
+ * @Version v1.0
+ */
+public class QueueConfig {
+}
