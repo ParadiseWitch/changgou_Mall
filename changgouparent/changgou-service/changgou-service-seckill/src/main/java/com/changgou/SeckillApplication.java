768fb2ea0e6c866fa6e0213a704b204a960720dd
Maiiiiiid <zhp92@outlook.com>
2020-09-24 22:48:41 +0800

    创建秒杀微服务

---
+++ changgouparent/changgou-service/changgou-service-seckill/src/main/java/com/changgou/SeckillApplication.java
@@ -0,0 +1,35 @@
+package com.changgou;
+
+import entity.IdWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @ClassName com.changgou.SeckillApplication
+ * @Description
+ * @Author Maid
+ * @Date 2020/9/24 0024 22:38
+ * @Version v1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@MapperScan(basePackages = {"com.changgou.seckill.dao"})
+@EnableScheduling
+public class SeckillApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeckillApplication.class,args);
+	}
+
+	@Bean
+	public IdWorker idWorker(){
+		return new IdWorker(1,1);
+	}
+}
