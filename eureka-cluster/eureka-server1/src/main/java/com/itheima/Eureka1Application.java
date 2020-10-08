d80b72fe8058a6b7d329c3b9fb1ba03add9dd2e3
Maiiiiiid <zhp92@outlook.com>
2020-10-08 11:03:14 +0800

    Eureka集群工程搭建

---
+++ eureka-cluster/eureka-server1/src/main/java/com/itheima/Eureka1Application.java
@@ -0,0 +1,30 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName com.itheima.Eureka1Application
+ * @Description
+ * @Author Maid
+ * @Date 2020/10/8 0008 10:44
+ * @Version v1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka1Application {
+
+	public static void main(String[] args) {
+		/**
+		 * @Description main
+		 * @param [args]
+		 * @return void
+		 * @auther Administrator
+		 * @date 2020/4/15
+		 */
+		SpringApplication.run(Eureka1Application.class,args);
+	}
+
+
+}
